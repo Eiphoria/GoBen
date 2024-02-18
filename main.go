58a00c635f8bae4dee15ae2f20187f41eaedd7e5
Eiphoria <spam.yakovich@gmail.com>
2024-02-18 12:23:24 +0200

    Add ability to play queue of songs. Minor code refactoring

--- main.go
+++ main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"bufio"
-	"context"
 	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
 	"log"
-	"log/slog"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"regexp"
 	"runtime"
 	"strconv"
@@ -28,14 +25,15 @@ const (
 	MAX_BYTES  int = (FRAME_SIZE * 2) * 2
 )
 
-func Infof(logger *slog.Logger, format string, args ...any) {
-	if !logger.Enabled(context.Background(), slog.LevelInfo) {
-		return
-	}
-	var pcs [1]uintptr
-	runtime.Callers(2, pcs[:]) // skip [Callers, Infof]
-	r := slog.NewRecord(time.Now(), slog.LevelInfo, fmt.Sprintf(format, args...), pcs[0])
-	_ = logger.Handler().Handle(context.Background(), r)
+func logMessage(message string, level string) {
+	_, fn, line, _ := runtime.Caller(1)
+	log.Printf("%s | %s:%d | [%s] | %s\n", getTimeStamp(), fn, line, level, message)
+	fmt.Printf("%s | %s:%d | [%s] | %s\n", getTimeStamp(), fn, line, level, message)
+}
+
+func getTimeStamp() string {
+	t := time.Now()
+	return t.Format("01/02/2006 15:04:05.000")
 }
 
 var queueMap = make(map[string]Queue)
@@ -50,7 +48,7 @@ func (q *Queue) Enqueue(item string) {
 
 func (q *Queue) Dequeue() (string, error) {
 	if len(q.items) == 0 {
-		return "", fmt.Errorf("Queue is empty")
+		return "", fmt.Errorf("queue is empty")
 	}
 	item := q.items[0]
 	q.items = q.items[1:]
@@ -68,143 +66,113 @@ func (q *Queue) ShowAll() ([]string, error) {
 }
 
 func main() {
-	replace := func(groups []string, a slog.Attr) slog.Attr {
-		// Remove time.
-		if a.Key == slog.TimeKey && len(groups) == 0 {
-			return slog.Attr{}
-		}
-		// Remove the directory from the source's filename.
-		if a.Key == slog.SourceKey {
-			source := a.Value.Any().(*slog.Source)
-			source.File = filepath.Base(source.File)
-		}
-		return a
+
+	f, err := os.OpenFile("file.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
 	}
-	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		AddSource:   true,
-		Level:       nil,
-		ReplaceAttr: replace,
-	}))
-	Infof(logger, "[INFO]: %s", "Launching GoBen...")
-	replace = func(groups []string, a slog.Attr) slog.Attr {
-		// Remove time.
-		if a.Key == slog.TimeKey && len(groups) == 0 {
-			return slog.Attr{}
-		}
-		// Remove the directory from the source's filename.
-		if a.Key == slog.SourceKey {
-			source := a.Value.Any().(*slog.Source)
-			source.File = filepath.Base(source.File)
-		}
-		return a
-	}
-	logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		AddSource:   true,
-		Level:       nil,
-		ReplaceAttr: replace,
-	}))
-	Infof(logger, "[INFO]: %s", "Launching GoBen...")
+	defer f.Close()
+	log.SetFlags(0)
+	log.SetOutput(f)
+
+	logMessage("Launching GoBen...", "INFO")
 	envVarName := "DPP_TOKEN"
 	token := os.Getenv(envVarName)
 	if token == "" {
 		fmt.Println("Cant retrieve env var: ", envVarName)
 		os.Exit(1)
-		// return
 	}
-	Infof(logger, "[INFO]: %s", "Pass token check")
+
+	logMessage("Pass token check", "INFO")
 	s, err := discordgo.New("Bot " + token)
 	if err != nil {
 		fmt.Println("error creating Discord session:", err)
 		return
 	}
 
-	Infof(logger, "[INFO]: %s", "Bot created")
+	logMessage("Bot created", "INFO")
 	defer s.Close()
 
-	// Register messageCreate as a callback for the messageCreate events.
-
-	Infof(logger, "[INFO]: %s", "Add handlers")
+	logMessage("Add handlers", "INFO")
 	s.AddHandler(messageCreate)
 
-	Infof(logger, "[INFO]: %s", "Open WebSocket")
+	logMessage("Open WebSocket", "INFO")
 	err = s.Open()
 	if err != nil {
-
-		fmt.Println("error opening connection:", err)
+		logMessage("error opening connection: "+err.Error(), "ERROR")
 		return
 	}
 
-	Infof(logger, "[INFO]: %s", "Bot Started")
+	logMessage("Bot Started", "INFO")
 	<-make(chan struct{})
 
 }
 
-// This function will be called (due to AddHandler above) every time a new
-// message is created on any channel that the autenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
-	// Ignore all messages created by the bot itself
-	// It's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
-	// check if the message contain ".play" command
 	if strings.HasPrefix(m.Content, ".play") {
-		queue := queueMap[m.GuildID]
-		// if queue.IsEmpty() {
-		// 	return
-		// }
-
 		url := extractYouTubeURL(m.Content)
 		if url == "" {
+			logMessage("Could not extract url ", "ERROR")
+
 			return
 		}
+		if _, ok := queueMap[m.GuildID]; !ok {
+			queueMap[m.GuildID] = Queue{}
+		}
+		queue := queueMap[m.GuildID]
 		queue.Enqueue(url)
-		fmt.Println("message: ", m.Content)
-		// Find the channel that the message came from.
+		queueMap[m.GuildID] = queue
+
+		voiceconn := s.VoiceConnections[m.GuildID]
+		if voiceconn != nil {
+			return
+		}
 		c, err := s.State.Channel(m.ChannelID)
 		if err != nil {
-			// Could not find channel.
-			fmt.Println("message: Could not find channel")
+			logMessage("Could not find channel: "+err.Error(), "ERROR")
 			return
 		}
-		fmt.Println("Channel found")
-		// Find the guild for that channel.
+
 		g, err := s.State.Guild(c.GuildID)
 		if err != nil {
-			// Could not find guild.
-			fmt.Println("message: Could not find guild")
+			logMessage("Could not find guild: "+err.Error(), "ERROR")
 			return
 		}
-		fmt.Println("Guild found")
-		// Look for the message sender in that guild's current voice states.
 		for _, vs := range g.VoiceStates {
 			if vs.UserID == m.Author.ID {
-				fmt.Println(vs.UserID, " == ", m.Author.ID)
-
-				if err = playSound(s, g.ID, vs.ChannelID, m.Content); err != nil {
-					log.Println("error playing sound:", err.Error())
+				if err = playSound(s, g.ID, vs.ChannelID); err != nil {
+					logMessage("Error playing sound: "+err.Error(), "ERROR")
 				}
-
 				return
 			}
 		}
-		fmt.Println(m.Author.ID, " not found in guild.VoiceStates")
+		logMessage(m.Author.ID+" not found in guild.VoiceStates.", "ERROR")
 
 	}
 	if strings.HasPrefix(m.Content, ".stop") {
-		// queue := queueMap[m.GuildID]
-		// if queue.IsEmpty() {
-		// 	return
-		// }
-		delete(queueMap, m.GuildID)
 		voiceconn := s.VoiceConnections[m.GuildID]
+		if voiceconn == nil {
+			return
+		}
+		delete(queueMap, m.GuildID)
 		voiceconn.Disconnect()
 	}
 
 	if strings.HasPrefix(m.Content, ".queue") {
-		queue := queueMap[m.GuildID]
+		queue, ok := queueMap[m.GuildID]
+		if !ok {
+			fmt.Println("No queue found for this guild.")
+			return
+		}
+
+		items, _ := queue.ShowAll()
+		str := strings.Join(items, "|\n")
+		logMessage(".queue cmd call,queue have entry: "+str, "DEBUG")
 		if queue.IsEmpty() {
 			return
 		}
@@ -236,94 +204,100 @@ func extractYouTubeURL(input string) string {
 	return ""
 }
 
-func playSound(s *discordgo.Session, guildID, channelID string, message string) error {
-	fmt.Println("Enter: func playSound")
-	// queue := queueMap[guildID]
-	// Join the provided voice channel.
-	url := extractYouTubeURL(message)
-	if url == "" {
-		return nil
-	}
-
-	vc, err := s.ChannelVoiceJoin(guildID, channelID, false, true)
-	if err != nil {
-		return err
-	}
-
-	defer vc.Disconnect()
-
-	ytdl := exec.Command("yt-dlp", "-v", "-f", "bestaudio", "-o", "-", url)
-
-	ytdlout, err := ytdl.StdoutPipe()
-	if err != nil {
-		return fmt.Errorf("ytdl stdout pipe: %w", err)
-	}
-	ytdlbuf := bufio.NewReaderSize(ytdlout, 16384)
-
-	ffmpeg := exec.Command("ffmpeg", "-i", "pipe:0", "-f", "s16le", "-ar", "48000", "-ac", "2", "pipe:1")
-	ffmpeg.Stdin = ytdlbuf
-	out, err := ffmpeg.StdoutPipe()
-	if err != nil {
-		return err
-	}
-
-	dca := exec.Command("dca", "pipe:0")
-	dca.Stdin = bufio.NewReaderSize(out, 1024)
-	dcaout, err := dca.StdoutPipe()
-	if err != nil {
-		return fmt.Errorf("dca stdout pipe: %w", err)
-	}
-
-	if err = ytdl.Start(); err != nil {
-		return fmt.Errorf("ytdl start: %w", err)
-	}
-
-	defer ytdl.Wait()
-
-	if err = ffmpeg.Start(); err != nil {
-		return fmt.Errorf("ffmpeg start: %w", err)
-	}
-
-	defer ffmpeg.Wait()
-
-	if err = dca.Start(); err != nil {
-		return fmt.Errorf("dca start: %w", err)
-	}
-
-	defer dca.Wait()
-
-	// header "buffer"
-	var opuslen int16
-
-	// Send "speaking" packet over the voice websocket
-	vc.Speaking(true)
-
-	// Send not "speaking" packet over the websocket when we finish
-	defer vc.Speaking(false)
-
-	dcaBuf := bufio.NewReaderSize(dcaout, 1024)
+func playSound(s *discordgo.Session, guildID, channelID string) error {
 	for {
-		if err = binary.Read(dcaBuf, binary.LittleEndian, &opuslen); err != nil {
-			if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
-				return nil
-			}
+		queue := queueMap[guildID]
 
-			return fmt.Errorf("binary read: %w", err)
+		vc, err := s.ChannelVoiceJoin(guildID, channelID, false, true)
+		if err != nil {
+			logMessage("error join voicechannel: "+err.Error(), "ERROR")
+			return fmt.Errorf("error join voicechannel: %w", err)
 		}
 
-		// read opus data from dca
-		opus := make([]byte, opuslen)
-		if err = binary.Read(dcaBuf, binary.LittleEndian, &opus); err != nil {
-			if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
+		url, err := queue.Dequeue()
+		if err != nil {
+			if err.Error() == "queue is empty" {
+				logMessage("Queue is empty. Ending playback.", "INFO")
+				vc.Speaking(false)
+				time.Sleep(time.Millisecond * 400)
+				vc.Disconnect()
+				delete(queueMap, guildID)
 				return nil
 			}
+		}
+		queueMap[guildID] = queue
 
-			if err != nil {
-				return fmt.Errorf("binary read: %w", err)
-			}
+		ytdl := exec.Command("yt-dlp", "-v", "-f", "bestaudio", "-o", "-", url)
+
+		ytdlout, err := ytdl.StdoutPipe()
+		if err != nil {
+			logMessage("ytdl stdout pipe: "+err.Error(), "ERROR")
+			return fmt.Errorf("ytdl stdout pipe: %w", err)
+		}
+		ytdlbuf := bufio.NewReaderSize(ytdlout, 16384)
+
+		ffmpeg := exec.Command("ffmpeg", "-i", "pipe:0", "-f", "s16le", "-ar", "48000", "-ac", "2", "pipe:1")
+		ffmpeg.Stdin = ytdlbuf
+		out, err := ffmpeg.StdoutPipe()
+		if err != nil {
+			logMessage("ffmpeg stdout pipe: "+err.Error(), "ERROR")
+			return fmt.Errorf("ffmpeg stdout pipe: %w", err)
 		}
 
-		// Send received PCM to the sendPCM channel
-		vc.OpusSend <- opus
+		dca := exec.Command("dca", "pipe:0")
+		dca.Stdin = bufio.NewReaderSize(out, 1024)
+		dcaout, err := dca.StdoutPipe()
+		if err != nil {
+			logMessage("dca stdout pipe: "+err.Error(), "ERROR")
+			return fmt.Errorf("dca stdout pipe: %w", err)
+		}
+
+		if err = ytdl.Start(); err != nil {
+			logMessage("ytdl start: "+err.Error(), "ERROR")
+			return fmt.Errorf("ytdl start: %w", err)
+		}
+
+		defer ytdl.Wait()
+
+		if err = ffmpeg.Start(); err != nil {
+			logMessage("ffmpeg start: "+err.Error(), "ERROR")
+			return fmt.Errorf("ffmpeg start: %w", err)
+		}
+
+		defer ffmpeg.Wait()
+
+		if err = dca.Start(); err != nil {
+			logMessage("dca start: "+err.Error(), "ERROR")
+			return fmt.Errorf("dca start: %w", err)
+		}
+
+		defer dca.Wait()
+
+		var opuslen int16
+
+		vc.Speaking(true)
+
+		dcaBuf := bufio.NewReaderSize(dcaout, 1024)
+		for {
+			if err = binary.Read(dcaBuf, binary.LittleEndian, &opuslen); err != nil {
+				if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
+					break
+				}
+				logMessage("binary read opuslen: "+err.Error(), "ERROR")
+				return fmt.Errorf("binary read opuslen: %w", err)
+			}
+
+			opus := make([]byte, opuslen)
+			if err = binary.Read(dcaBuf, binary.LittleEndian, &opus); err != nil {
+				if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
+					break
+				}
+				if err != nil {
+					logMessage("binary read opus: "+err.Error(), "ERROR")
+					return fmt.Errorf("binary read opus: %w", err)
+				}
+			}
+			vc.OpusSend <- opus
+		}
 	}
 }
